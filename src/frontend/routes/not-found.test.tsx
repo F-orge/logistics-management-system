5685ad2c115bbdd29aa454d15f7bba7d9c544b92
karlrobeck <karlalferezfx@gmail.com>
2024-12-02 22:21:04 +0800

    remove user event

--- src/frontend/routes/not-found.test.tsx
+++ src/frontend/routes/not-found.test.tsx
@@ -1,9 +1,6 @@
 import NotFoundPage from "./not-found.tsx";
 import { expect, test } from "vitest";
 import { render } from "@solidjs/testing-library";
-import userEvent from "@testing-library/user-event";
-
-const user = userEvent.setup();
 
 test("renders not found page", () => {
   const { getByText } = render(() => <NotFoundPage />);
