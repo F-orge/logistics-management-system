c83b885f40ee186190e4d90404898efe0d4df8b8
karlrobeck <karlalferezfx@gmail.com>
2024-12-29 04:23:28 +0800

    add sea orm as a replacement to kysely

--- migration/src/m20241228_193306_user.rs
+++ migration/src/m20241228_193306_user.rs
@@ -1,3 +1,5 @@
+use extension::postgres::Type;
+use sea_orm::{EnumIter, Iterable};
 use sea_orm_migration::{prelude::*, schema::*};
 
 #[derive(DeriveMigrationName)]
@@ -6,6 +8,15 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_type(
+                Type::create()
+                    .as_enum(RoleEnum)
+                    .values(RoleVariants::iter())
+                    .to_owned(),
+            )
+            .await?;
+
         manager
             .create_table(
                 Table::create()
@@ -24,7 +35,11 @@ impl MigrationTrait for Migration {
                             .not_null(),
                     )
                     .col(ColumnDef::new(User::Password).string_len(128).not_null())
-                    .col(ColumnDef::new(User::Role).uuid().not_null().primary_key())
+                    .col(
+                        ColumnDef::new(User::Role)
+                            .enumeration(Alias::new("role_enum"), RoleVariants::iter())
+                            .not_null(),
+                    )
                     .col(
                         ColumnDef::new(User::CreatedAt)
                             .timestamp()
@@ -44,6 +59,10 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
+            .drop_type(Type::drop().name(RoleEnum).to_owned())
+            .await?;
+
+        manager
             .drop_table(Table::drop().table(User::Table).to_owned())
             .await
     }
@@ -58,4 +77,15 @@ enum User {
     Role,
     CreatedAt,
     UpdatedAt,
+}
+
+#[derive(DeriveIden)]
+struct RoleEnum;
+
+#[derive(DeriveIden, EnumIter)]
+enum RoleVariants {
+    SuperAdmin,
+    Admin,
+    Employee,
+    Client,
 }
--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,6 +1,12 @@
 use sea_orm_migration::prelude::*;
 
-#[async_std::main]
+#[tokio::main]
 async fn main() {
+    if cfg!(debug_assertions) {
+        dotenv::from_filename(".env.development").ok();
+    } else {
+        dotenv::from_filename(".env.production").ok();
+    }
+
     cli::run_cli(migration::Migrator).await;
 }
