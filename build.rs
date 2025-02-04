6d5ab67031fde2c36bd5df3610ea30b14e02eadb
karlrobeck <karlalferezfx@gmail.com>
2025-02-05 02:41:01 +0800

    add crate-proto

--- build.rs
+++ build.rs
@@ -1,16 +1,17 @@
 fn main() {
     tonic_build::configure()
-        .out_dir("src/models/_proto")
+        .out_dir("crate-proto/src")
+        .compile_well_known_types(true)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
-                "proto/health_check.proto",
-                "proto/employee_management.proto",
-                "proto/auth.proto",
-                "proto/file_management.proto",
-                "proto/storage.proto",
+                "crate-proto/proto/health_check.proto",
+                "crate-proto/proto/employee_management.proto",
+                "crate-proto/proto/auth.proto",
+                "crate-proto/proto/file_management.proto",
+                "crate-proto/proto/storage.proto",
             ],
-            &["proto/"],
+            &["crate-proto/proto/"],
         )
         .unwrap();
 }
