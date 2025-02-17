ae2108a94c13d31f1f617cc85fe7ec1a0a5e5f3b
karlrobeck <karlalferezfx@gmail.com>
2025-02-17 14:39:20 +0800

    add triggers

--- migrations/20250216171737_management_employee.up.sql
+++ migrations/20250216171737_management_employee.up.sql
@@ -6,14 +6,19 @@ create schema management;
 -- reference: https://in.indeed.com/career-advice/starting-new-job/employee-information-form
 create type management.employee_marital_status as enum ('Single','Married','Divorced', 'Widowed', 'Separated');
 
+create type management.employee_role as enum ('Admin','Manager','Employee');
+
 create table management.employee
 (
-    id                   uuid primary key        default gen_random_uuid(),
+    id                   uuid primary key                  default gen_random_uuid(),
     -- note:
     -- full name must be unique so no duplicate information will be processed
-    full_name            varchar(255)   not null unique,
+    first_name           varchar(255)             not null,
+    middle_name          varchar(255)             not null,
+    last_name            varchar(255)             not null,
+    constraint unique_employee_name unique (first_name, middle_name, last_name),
     -- references: https://en.wikipedia.org/wiki/Telephone_numbers_in_the_Philippines#:~:text=Mobile%20phone%20numbers%20are%20always,a%20seven%2Ddigit%20number).
-    tel_number           varchar(10)    null,
+    tel_number           varchar(10)              null,
     /*
         Mobile numbers in the Philippines are 10 or 12 digits long.
         Local mobile numbers
@@ -21,21 +26,25 @@ create table management.employee
             Can be 12 digits long, such as 639181234567
             Do not include special characters like + - . #
     */
-    mobile_number        varchar(12)    null check (mobile_number ~ '^(09[0-9]{9}|639[0-9]{9})$'),
+    mobile_number        varchar(12)              null check (mobile_number ~ '^(09[0-9]{9}|639[0-9]{9})$'),
     -- note:
     -- this will be the same in auth.basic_user. if auth_type is different use any email
-    email                varchar(255)   not null unique check ( email ~ '^.+@.+\..+$' ),
+    email                varchar(255) unique check ( email ~ '^.+@.+\..+$' ),
+    role                 management.employee_role not null default 'Employee',
     -- note:
     -- philippine national id format is xxxx-xxxx-xxxx-xxxx
-    phil_nat_id          varchar(19)    not null check (phil_nat_id ~ '^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$'),
-    birth_date           date           not null check ( birth_date <= current_date - interval '18 years' ),
-    special_interests    varchar(255)[] null,
-    learning_institution varchar(255)[] not null,
+    phil_nat_id          varchar(19)              not null check (phil_nat_id ~ '^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$'),
+    birth_date           date                     not null check ( birth_date <= current_date - interval '18 years' ),
+    special_interests    varchar(255)[]           null,
+    learning_institution varchar(255)[]           not null,
     auth_user_id         uuid references auth.users (id),
-    spouse_name          varchar(255)   null,
-    spouse_employer      varchar(255)   null,
-    created_at           timestamp      not null default current_timestamp,
-    updated_at           timestamp      not null default current_timestamp
+    spouse_first_name    varchar(255)             null,
+    spouse_middle_name   varchar(255)             null,
+    spouse_last_name     varchar(255)             null,
+    constraint unique_spouse_name unique (spouse_first_name, spouse_middle_name, spouse_last_name),
+    spouse_employer      varchar(255)             null,
+    created_at           timestamp                not null default current_timestamp,
+    updated_at           timestamp                not null default current_timestamp
 );
 
 create table management.department
@@ -100,6 +109,35 @@ create table management.personnel_action
     approved_by    uuid                         not null references management.employee (id),
     created_at     timestamp                    not null default current_timestamp,
     updated_at     timestamp                    not null default current_timestamp
-)
+);
 
 -- triggers
+create function management.create_basic_user_employee_trigger_fn() returns trigger as
+$$
+declare
+    _email    varchar(255);
+    _password varchar(255);
+begin
+
+    -- password format. last_name.m/d/y
+    _password = new.last_name || date_part('month', new.birth_date) || date_part('day', new.birth_date) ||
+                date_part('year', new.birth_date);
+
+    if new.email is null then
+        -- create a basic user with email format of last_name.random_number@domain
+        _email := new.last_name || floor(random() * 100000)::int || current_setting('app.management.domain');
+    else
+        _email := new.email;
+    end if;
+
+    insert into auth.basic_user(email, password) values (_email, _password);
+
+    return new;
+end
+$$ language plpgsql;
+
+create trigger create_basic_user_employee_trigger
+    before insert
+    on management.employee
+    for each row
+execute function management.create_basic_user_employee_trigger_fn();
